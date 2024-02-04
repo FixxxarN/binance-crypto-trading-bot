f35790b8e86547357777744084f5375620c34edd
FixxxarN <tornstromandre@gmail.com>
2024-02-04 15:19:03 +0100

    #1 create login endpoint using express and create bot when logging in

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,22 @@
 const express = require('express');
+const BotManager = require('./botManager');
+const cors = require('cors');
 const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+  origin: '*'
+}))
+
+const botManager = new BotManager();
+
+app.post('/login', (req, res) => {
+  const { apiKey, apiSecret } = req.body;
+
+  const botId = botManager.createBot({ apiKey, apiSecret });
+
+  res.send({ botId });
+});
 
 app.listen(3001);

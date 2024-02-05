7c885ed07e0883d4880a137f49f4c7bacbf846d6
FixxxarN <tornstromandre@gmail.com>
2024-02-05 14:31:03 +0100

    Fix failing tests after bot refactor

--- backend/tests/botManager.test.js
+++ backend/tests/botManager.test.js
@@ -1,48 +1,66 @@
 const BotManager = require("../botManager");
+const Bot = require('../bot');
 
 describe('BotManager', () => {
+  beforeEach(() => {
+    jest.spyOn(Bot.prototype, 'connectToBinance').mockImplementation(() => true);
+  });
+
   it('should have an empty array of bots when created', () => {
     const botManager = new BotManager();
     expect(botManager.bots).toHaveLength(0);
   });
 
   describe('createBot', () => {
-    it('should add a bot to the array of bots', () => {
+    it('should add a bot to the array of bots', async () => {
+      jest.spyOn(Bot.prototype, 'checkIfCredentialsAreValid').mockImplementation(() => true);
+
       const botManager = new BotManager();
-      botManager.createBot('fakeKey', 'fakeSecret');
+
+      await botManager.createBot('fakeKey', 'fakeSecret');
+
       expect(botManager.bots).toHaveLength(1);
     });
   });
 
   describe('findBotById', () => {
-    it('should find a bot by id', () => {
-      const botManager = new BotManager();
-      const createdBotId = botManager.createBot('fakeKey', 'fakeSecret')
+    it('should find a bot by id', async () => {
+      jest.spyOn(Bot.prototype, 'checkIfCredentialsAreValid').mockImplementation(() => true);
 
-      const foundBot = botManager.findBotById(createdBotId);
+      const botManager = new BotManager();
+
+      const createdBotId = await botManager.createBot('fakeKey', 'fakeSecret')
+      const foundBot = await botManager.findBotById(createdBotId);
 
       expect(foundBot).not.toBeUndefined();
     });
 
-    it('should throw error if no bot found', () => {
-      const botManager = new BotManager();
-      botManager.createBot('fakeKey', 'fakeSecret')
+    it('should throw error if no bot found', async () => {
+      jest.spyOn(Bot.prototype, 'checkIfCredentialsAreValid').mockImplementation(() => { throw new Error(); });
 
-      const foundBot = botManager.findBotById('fakeId');
+      const botManager = new BotManager();
+
+      await botManager.createBot('fakeKey', 'fakeSecret')
+      const foundBot = await botManager.findBotById('fakeId');
 
       expect(foundBot).toBeUndefined();
     });
   });
 
   describe('terminateBot', () => {
-    it('should return true and remove the bot if it can find it in the array', () => {
+    it('should return true and remove the bot if it can find it in the array', async () => {
+      jest.spyOn(Bot.prototype, 'checkIfCredentialsAreValid').mockImplementation(() => true);
+
       const botManager = new BotManager();
-      const botId = botManager.createBot('fakeKey', 'fakeSecret')
+      const botId = await botManager.createBot('fakeKey', 'fakeSecret')
 
       expect(botManager.terminateBot(botId)).toBeTruthy();
     });
 
     it('should return false if it doesnt find the bot', () => {
+      jest.spyOn(Bot.prototype, 'checkIfCredentialsAreValid').mockImplementation(() => { throw new Error(); });
+
+
       const botManager = new BotManager();
 
       botManager.createBot('fakeKey', 'fakeSecret')
